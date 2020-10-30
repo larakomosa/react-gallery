5c252a913fdb1f6353ccb6f8faa018a8dc72e55f
Lara Komosa <larakomosa@gmail.com>
2020-10-30 16:09:19 -0500

    files setup, working on app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    galleryItem: {
+      id: '',
+      path: '',
+      description: '',
+      likes: '',
+    },
+    galleryList: [],
+    errorMsg: null,
+  };
+
+  getImages() {
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    })
+      .then((response) => {
+        // {
+        //   data: []
+        // }
+        console.log(response);
+        this.setState({
+          songs: response.data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({
+          errorMsg: 'Something went terribly wrong.',
+        });
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of my life</h1>
         </header>
-        <br/>
+        <br />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" />
       </div>
     );
   }
